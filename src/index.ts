0e4d02b49dce158e133934762b69b2ac3fffc53f
Felipe Moura <felipemoura@DESKTOP-Q6PMQI4.localdomain>
2021-04-01 13:11:40 -0300

    Criação do endpoint para pegar as informações do estudante

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,12 @@ import  { newClass } from "./endpoints/createClass";
 import { createStudent } from "./endpoints/createStudent";
 import { createTeacher } from "./endpoints/createTeacher";
 import { getALlStudent } from "./endpoints/getAllStudent";
+import { getInfoStudent } from "./endpoints/getInfoStudent";
 
 
 app.get('/student', getALlStudent)
-
 app.post('/student', createStudent)
+app.get('/stundet/info/:id', getInfoStudent)
 app.post('/teacher', createTeacher)
 app.post('/class', newClass)
 app.post('/class/addstudent', addStudent)
